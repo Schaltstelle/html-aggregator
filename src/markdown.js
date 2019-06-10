e968c87d77f902ce245d58c0647c55a11f76b1e8
Stefan Niederhauser <ghuder5@gmx.ch>
2019-06-10 19:32:26 +0200

    support path property in markdown/templates add priority to processors optionally process underscore files in processors

--- src/markdown.js
+++ src/markdown.js
@@ -1,36 +1,36 @@
-'use strict';
-const marked = require('marked');
-const path = require('path');
-const fs = require('fs');
-const yaml = require('./yaml');
-const template = require('./template');
+'use strict'
+const marked = require('marked')
+const path = require('path')
+const fs = require('fs')
+const yaml = require('./yaml')
+const template = require('./template')
 
 module.exports = {
     run: run
-};
+}
 
 function run(input, data) {
-    let split = /^---\s*$/m.exec(input);
+    let split = /^---\s*$/m.exec(input)
     let parts = (split === null)
         ? [input, '']
-        : [input.substring(0, split.index), input.substring(split.index + split[0].length)];
+        : [input.substring(0, split.index), input.substring(split.index + split[0].length)]
     return yaml.load(parts[0]).then(y => {
-        let fullData = Object.assign({}, y, data);
-        fullData.content = marked(parts[1]);
-        let templ = findTemplate(fullData);
+        let fullData = Object.assign({}, y, data)
+        fullData.content = marked(parts[1])
+        let templ = findTemplate(fullData)
         return templ
             ? template.run(templ.text, fullData).then(res => {
-                return {data: res.data, ext: templ.ext};
+                return {data: res.data, path: fullData.path, ext: templ.ext}
             })
-            : {data: fullData, ext: '.html'};
-    });
+            : {data: fullData, path: fullData.path, ext: '.html'}
+    })
 }
 
 function findTemplate(data) {
     if (data.template) {
-        return {text: data.template, ext: '.html'}; //TODO a way to find out extname of !include?
+        return {text: data.template, ext: '.html'} //TODO a way to find out extname of !include?
     }
     if (data.templateFile) {
-        return {text: fs.readFileSync(data.templateFile, 'utf8'), ext: path.extname(data.templateFile)};
+        return {text: fs.readFileSync(data.templateFile, 'utf8'), ext: path.extname(data.templateFile)}
     }
 }
--- src/processors.js
+++ src/processors.js
@@ -1,76 +1,82 @@
-'use strict';
+'use strict'
 
-const debug = require('debug')('processors');
-const chalk = require('chalk');
-const template = require('./template');
-const markdown = require('./markdown');
-const configs = require('./configs');
-const glob = require('glob');
-const path = require('path');
-const fs = require('fs');
-const fse = require('fs-extra');
-const chokidar = require('chokidar');
-const server = require('http-server');
+const debug = require('debug')('processors')
+const chalk = require('chalk')
+const template = require('./template')
+const markdown = require('./markdown')
+const configs = require('./configs')
+const glob = require('glob')
+const path = require('path')
+const fs = require('fs')
+const fse = require('fs-extra')
+const chokidar = require('chokidar')
+const server = require('http-server')
 
-let procs = [];
+let procs = []
 
 module.exports = {
     registerProcessor: registerProc,
     run: function (file) {
-        return file ? runProc(file) : runAll();
+        return file ? runProc(file) : runAll()
     },
     processorFor: findProc
-};
+}
 
-registerProc('Markdown', '\.(md|yml|yaml)$', true, markdown.run);
+registerProc('Markdown', '\.(md|yml|yaml)$', markdown.run, {priority: -100})
 
-registerProc('Template', '\.html$', true, template.run);
+registerProc('Template', '\.html$', template.run, {priority: -100})
 
-registerProc('Copy', '', false, (input) => {
-    return Promise.resolve({data: input});
-});
+registerProc('Copy', '', input => Promise.resolve({data: input}), {binaryInput: true, priority: -200})
 
 function runAll() {
     return new Promise((resolve, reject) => {
-        let ignore = ['node_modules/**', configs.args.outputDir + '/**', '**/_*/**', '**/_*'].concat(configs.args.exclude);
+        let ignore = ['node_modules/**', configs.args.outputDir + '/**'].concat(configs.args.exclude)
         glob('**', {
             nodir: true,
             ignore: ignore
         }, (err, files) => {
             if (err) {
-                reject(err);
+                reject(err)
             }
             resolve(Promise.all(
                 files.map(file => runProc(file))).then(() => {
-                    debug('Processed %d resources', files.length);
+                    debug('Processed %d resources', files.length)
                     if (configs.args.watch) {
-                        serve(8111);
-                        watch(ignore);
+                        serve(8111)
+                        watch(ignore)
                     }
                 })
-            );
-        });
-    });
+            )
+        })
+    })
 }
 
-function registerProc(name, test, textInput, exec) {
-    procs.push({name: name, test: test, textInput: textInput, exec: exec});
+function registerProc(name, test, exec, options) {
+    procs.push({name, test, exec, priority: 0, ...options})
+    procs.sort((a, b) => a.priority < b.priority ? 1 : a.priority > b.priority ? -1 : 0)
 }
 
 function runProc(file) {
     //TODO fails on delete event
-    return execProc(findProc(file), file);
+    let proc = findProc(file)
+    return !proc.underscoreFiles && (file.substring(0,1)==='_' || file.indexOf('/_')>=0) ? Promise.resolve('ignored') : execProc(proc, file)
 }
 
 function findProc(file) {
-    return procs.find(p => file.match(p.test));
+    return procs.find(p => file.match(p.test))
 }
 
 function execProc(proc, file) {
-    let data = fs.readFileSync(file, proc.textInput ? 'utf8' : {})
+    let data = fs.readFileSync(file, proc.binaryInput ? {} : 'utf8')
     return proc.exec(data)
         .then(res => {
-            let outParts = path.parse(path.resolve(configs.args.outputDir, file))
+            let outPath = file
+            if (res.path) {
+                outPath = res.path.substring(res.path.length - 1) === '/'
+                    ? res.path + path.parse(file).base
+                    : res.path
+            }
+            let outParts = path.parse(path.resolve(configs.args.outputDir, outPath))
             fse.mkdirsSync(outParts.dir)
             let outName = path.resolve(outParts.dir, outParts.name + (res.ext ? res.ext : outParts.ext))
             fs.writeFileSync(outName, res.data)
@@ -84,18 +90,18 @@ function execProc(proc, file) {
 
 function serve(port) {
     server.createServer({root: configs.args.outputDir, cache: -1}).listen(port, '127.0.0.1', () => {
-        debug('Serving at', chalk.blue.underline('http://localhost:' + port));
-    });
+        debug('Serving at', chalk.blue.underline('http://localhost:' + port))
+    })
 }
 
 function watch(ignore) {
-    let ignored = ignore.concat(['**/.*', '**/.*/**', configs.args.outputDir]);
+    let ignored = ignore.concat(['**/.*', '**/.*/**', configs.args.outputDir])
     chokidar.watch('', {
         ignoreInitial: true,
         ignored: ignored
     }).on('ready', () => {
-        debug('Watching for changes...');
+        debug('Watching for changes...')
     }).on('all', (event, file) => {
-        runProc(file);
-    }).on('error', (err) => debug(err));
+        runProc(file)
+    }).on('error', (err) => debug(err))
 }
